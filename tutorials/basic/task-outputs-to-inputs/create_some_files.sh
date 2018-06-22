8fe1c8950247dd3791c68aa72045722a1bae2f7c
Harshal Shah <hsh@hellofresh.com>
2018-06-22 10:55:39 +0200

    Testing stupid changes

--- tutorials/basic/task-outputs-to-inputs/create_some_files.sh
+++ tutorials/basic/task-outputs-to-inputs/create_some_files.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkdir some-files
-echo "file1" > some-files/file1
-echo "file2" > some-files/file2
-echo "file3" > some-files/file3
-echo "file4" > some-files/file4
+mkdir some-files2
+echo "file1" > some-files2/file1
+echo "file2" > some-files2/file2
+echo "file3" > some-files2/file3
+echo "file4" > some-files2/file4
 
-ls some-files/*
+ls some-files2/*
